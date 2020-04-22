e361c03a66f65f52683525298e7d8ecda53fed48
jalaniz-sys <60115425+jalaniz-sys@users.noreply.github.com>
2020-04-21 21:26:59 -0500

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function init()
+{
+
+	var msg=document.getElementById('entryinput');
+	var button=document.getElementById('entrybutton');
+	var screen=document.getElementById('textoutput');
+	var joxh=document.getElementsByClassName('center')[1];
+
+	function alertUser()
+	{
+		alert(joxh.textContent+': '+msg.value);
+	}
+
+	function msgOutput()
+	{
+		screen.innerHTML=msg.value;
+	}
+	button.addEventListener('click', alertUser);
+	button.addEventListener('click',msgOutput);
+}
+
+}
